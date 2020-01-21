ed6c51013a8254bb568872a47c436f1d90b54a9b
dawidja94 <dawidjasionowski@lewisu.edu>
2020-01-21 14:41:30 -0600

    latest entity update

--- RestaurantWebAPI/RestaurantWebAPI/Models/Entities/CarryOut.cs
+++ RestaurantWebAPI/RestaurantWebAPI/Models/Entities/CarryOut.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,12 +12,16 @@ namespace RestaurantWebAPI.Models.Entities
         [Key]
         public int Id { get; set; }
        public int BundleId { get; set; }
+        
+        [ForeignKey("CustomerId")]
         public int CustomerId { get; set; }
-        [Required]
-        public int FoodId { get; set; }
+        
+        [ForeignKey("FoodId")]
+        public Food FoodId { get; set; }
         [Required]
         public int FoodQuanity { get; set; }
-        public int BeverageID { get; set; }
+        [ForeignKey("BeverageID")]
+        public Beverage BeverageID { get; set; }
         [Required]
         public int BeverageQuantity { get; set; }
         public DateTime SubmissionTime { get; set; }
--- RestaurantWebAPI/RestaurantWebAPI/Startup.cs
+++ RestaurantWebAPI/RestaurantWebAPI/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestaurantWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace RestaurantWebAPI
 {
@@ -25,6 +28,8 @@ namespace RestaurantWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("AzureConnection")));
             services.AddControllers();
         }
 
