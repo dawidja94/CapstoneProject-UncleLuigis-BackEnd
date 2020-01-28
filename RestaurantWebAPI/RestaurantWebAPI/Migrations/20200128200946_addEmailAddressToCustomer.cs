f1307e2b50c3f010891aff6e52334a1ff7eda59a
dawidja94 <dawidjasionowski@lewisu.edu>
2020-01-28 15:06:50 -0600

    added migrations and GetAllCustomersResponse

---
+++ RestaurantWebAPI/RestaurantWebAPI/Migrations/20200128200946_addEmailAddressToCustomer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantWebAPI.Migrations
+{
+    public partial class addEmailAddressToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Customers",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Customers");
+        }
+    }
+}
